a5749b3ae9b9e2a9168a3be8e647e90435f6e981
wwervin72 <2411120974@qq.com>
2017-02-12 23:43:02 +0800

    login register

--- backend/config/default.js
+++ backend/config/default.js
@@ -1,6 +1,6 @@
 module.exports = {
 	mongoDB: 'mongodb://localhost:8080/myBlog',
-	port: 3000,
+	port: 3030,
 	session: {
 		key: 'myBlog',
 		secret: 'myblog',
@@ -10,6 +10,6 @@ module.exports = {
 	github: {
 		clientID: 'cb448b1d4f0c743a1e36',
 		clientSecret: '815aa4606f476444691c5f1c16b9c70da6714dc6',
-		callbackURL: 'http://localhost:3000/auth/github/callback'
+		callbackURL: 'http://localhost:3030/auth/github/callback'
 	}
 };
--- frontend/src/modules/app/controllers/app.js
+++ frontend/src/modules/app/controllers/app.js
@@ -5,5 +5,28 @@ define(['angular'], (angular) => {
 	let dependency = [];
 	let appModule = angular.module('app', dependency);
 	appModule.controller('ctrl', appCtrl);
+	appModule.config(($stateProvider, $urlRouterProvider) => {
+		$stateProvider
+			.state('home', {
+				url: '/',
+				templateUrl: '../views/home.html',
+				controller: 'home.ctrl'
+			})
+			.state('user', {
+				url: '/',
+				templateUrl: '../views/home.html',
+				controller: 'home.ctrl'
+			})
+			.state('user.login', {
+				url: '/login',
+				templateUrl: '../../user/views/login.html',
+				controller: 'user.login.ctrl'
+			})
+			.state('user.register', {
+				url: '/register',
+				templateUrl: '../views/home.html',
+				controller: 'home.ctrl'
+			})
+	})
 	return appModule;
 });
